ef565e9cfdeadb8320392c558516874c289b7576
mythicalbadger <jussy12.work@gmail.com>
2024-03-16 15:12:06 +0700

    refactor: added student links, flags table, favorites table

--- Internships Diagram.sql
+++ Internships Diagram.sql
@@ -3,13 +3,12 @@ CREATE TABLE internships
     id             integer NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'internship id',
     company        varchar(255) COMMENT 'Agoda',
     position       varchar(255) COMMENT 'Backend, Frontend, Fullstack',
-    skill_required varchar(255) COMMENT 'scala, sql, docker',
     website        varchar(255) COMMENT 'www.agoda.com',
     deadline       date COMMENT '2024-03-31 or NULL for no deadline',
     time_period_id integer FOREIGN KEY REFERENCES time_periods(id),
     author_id      integer FOREIGN KEY REFERENCES users(id),
     flagged        boolean DEFAULT false COMMENT 'whether or not internship flagged',
-    flagged_amount integer DEFAULT 0 COMMENT 'to order by most flagged'
+    created_at     timestamp DEFAULT CURRENT_TIMESTAMP COMMENT 'when internship object was created',
 );
 
 CREATE TABLE time_periods
@@ -19,7 +18,6 @@ CREATE TABLE time_periods
     start_date date         NOT NULL,
     end_date   date         NOT NULL
 );
-
 
 CREATE TABLE roles
 (
@@ -36,5 +34,24 @@ CREATE TABLE users
     password                  varchar(255) COMMENT 'hashed password',
     role_id                   integer DEFAULT 0 FOREIGN KEY REFERENCES roles(id),
     gpa                       float   DEFAULT null,
+    academic_year             integer DEFAULT null,
+    github_link               varchar(255),
+    linkedin_link             varchar(255),
+    website_link              varchar(255),
+    phone_number              varchar(255),
     internship_time_period_id integer FOREIGN KEY REFERENCES time_periods(id),
+);
+
+CREATE TABLE favorites
+(
+    user_id       integer FOREIGN KEY REFERENCES users(id),
+    internship_id integer FOREIGN KEY REFERENCES internships(id),
+    PRIMARY KEY (user_id, internship_id)
+);
+
+CREATE TABLE flags
+(
+    user_id       integer FOREIGN KEY REFERENCES users(id),
+    internship_id integer FOREIGN KEY REFERENCES internships(id),
+    PRIMARY KEY (user_id, internship_id)
 );
